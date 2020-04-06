be3e735de47f0aa937dd9dab19272e4bf9b5e690
Kyaw Kyaw Lwin <kyawkyawlwinthant@gmail.com>
2020-04-06 20:07:41 +0630

    reactive app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {fromEvent} from 'rxjs';
+import {tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-app';
+
+  constructor() {
+
+    const key$=fromEvent(document,'keyup')
+      .pipe(
+        tap((keyup:KeyboardEvent) => console.log('From Observable',keyup))
+      );
+
+    key$.subscribe(
+      data => console.log('From Consumer ',data)
+    )
+  }
 }
